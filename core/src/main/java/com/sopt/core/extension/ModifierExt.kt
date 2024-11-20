aaf4e55030cdfe8f833683aed97b6f6ed9fcb481
gaeulzzang <gaeulzzang@gmail.com>
2024-11-20 18:31:22 +0900

    #3 [ADD] modifier 확장 함수 추가

--- core/src/main/java/com/sopt/core/extension/ModifierExt.kt
+++ core/src/main/java/com/sopt/core/extension/ModifierExt.kt
@@ -2,13 +2,18 @@ package com.sopt.core.extension
 
 import android.annotation.SuppressLint
 import android.graphics.BlurMaskFilter
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
@@ -85,5 +90,27 @@ fun Modifier.customShadow(
                 )
             }
         }
+    }
+}
+
+fun Modifier.roundedBackgroundWithPadding(
+    backgroundColor: Color,
+    cornerRadius: Dp,
+    padding: Dp
+): Modifier {
+    return this
+        .background(backgroundColor, shape = RoundedCornerShape(cornerRadius))
+        .padding(padding)
+}
+
+fun Modifier.showIf(condition: Boolean): Modifier {
+    return if (condition) this else Modifier.size(0.dp)
+}
+
+fun Modifier.animateVisibility(isVisible: Boolean): Modifier {
+    return if (isVisible) {
+        this.alpha(1f)
+    } else {
+        this.alpha(0f)
     }
 }
