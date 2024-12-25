62df2633239627583d4663a37b17f7fd381f5568
gaeulzzang <gaeulzzang@gmail.com>
2024-12-26 00:09:08 +0900

    #3 [FEAT] 컬러시스템 구춧

--- core/src/main/java/com/sopt/core/designsystem/theme/Color.kt
+++ core/src/main/java/com/sopt/core/designsystem/theme/Color.kt
@@ -2,10 +2,35 @@ package com.sopt.core.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Uncategorized
+val Black = Color(0xFF111111)
+val White = Color(0xFFFFFFFF)
+val Red = Color(0xFF9C0303)
+val Pink = Color(0xFFFFCACA)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Gray Scale
+val Gray50 = Color(0xFFF8F8FA)
+val Gray100 = Color(0xFFEFF1F3)
+val Gray200 = Color(0xFFE9E9EB)
+val Gray500 = Color(0xFFB1B1B1)
+val Gray600 = Color(0xFF999999)
+val Gray700 = Color(0xFF808080)
+val Gray800 = Color(0xFF333333)
+val Gray900 = Color(0xFF222222)
+
+// Chip Color
+val Blue = Color(0xFF3E8EFF)
+val Purple = Color(0xFF8D78D8)
+val Orange = Color(0xFFFF7826)
+val Mint = Color(0xFFA9DBBE)
+
+// Blue Scale
+val Blue50 = Color(0xFFF0F4FF)
+val Blue100 = Color(0xFFECF0FF)
+val Blue200 = Color(0xFFCDD9FF)
+val Blue300 = Color(0xFFB4C4FF)
+val Blue400 = Color(0xFF9DAFE9)
+val Blue500 = Color(0xFF7790ED)
+val Blue600 = Color(0xFF7288CF)
+val Blue700 = Color(0xFF7288CF)
+val Blue800 = Color(0xFF49567F)
