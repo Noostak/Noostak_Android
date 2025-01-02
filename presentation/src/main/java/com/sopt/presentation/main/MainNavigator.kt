f98c66926d3866714b9ce7efd97b99a13d86e4a1
youjin09222 <youjin09222@naver.com>
2025-01-03 00:09:41 +0900

    #6 [REFACTOR] MainNavigator 초기 경로 Login으로 변경

--- presentation/src/main/java/com/sopt/presentation/main/MainNavigator.kt
+++ presentation/src/main/java/com/sopt/presentation/main/MainNavigator.kt
@@ -9,11 +9,10 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navOptions
-import com.sopt.presentation.calendar.navigation.Calendar
 import com.sopt.presentation.calendar.navigation.navigateCalendar
 import com.sopt.presentation.group.navigation.navigateGroup
 import com.sopt.presentation.mypage.navigation.navigateMyPage
-
+import com.sopt.presentation.auth.login.navigation.Login
 class MainNavigator(
     val navController: NavHostController
 ) {
@@ -21,7 +20,7 @@ class MainNavigator(
         @Composable get() = navController
             .currentBackStackEntryAsState().value?.destination
 
-    val startDestination = Calendar
+    val startDestination = Login
 
     val currentTab: MainTab?
         @Composable get() = MainTab.find { tab ->
