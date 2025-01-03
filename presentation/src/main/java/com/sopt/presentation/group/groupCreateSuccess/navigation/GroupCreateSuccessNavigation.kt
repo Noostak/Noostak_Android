4f5f0b89cdf9a11a8a397519bdc9cc8e7baa2ecd
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 06:01:48 +0900

    #10 [FEAT] NavGraph BackStack 관리

--- presentation/src/main/java/com/sopt/presentation/group/groupCreateSuccess/navigation/GroupCreateSuccessNavigation.kt
+++ presentation/src/main/java/com/sopt/presentation/group/groupCreateSuccess/navigation/GroupCreateSuccessNavigation.kt
@@ -5,6 +5,7 @@ import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.sopt.core.navigation.Route
+import com.sopt.presentation.group.groupCreate.navigation.GroupCreate
 import com.sopt.presentation.group.groupCreateSuccess.GroupCreateSuccessRoute
 import com.sopt.presentation.groupDetail.navigation.navigateGroupDetail
 import kotlinx.serialization.Serializable
@@ -12,7 +13,9 @@ import kotlinx.serialization.Serializable
 fun NavController.navigateToGroupCreateSuccess(navOptions: NavOptions? = null) {
     navigate(
         route = GroupCreateSuccess,
-        navOptions = navOptions
+        navOptions = navOptions ?: NavOptions.Builder()
+            .setPopUpTo(GroupCreate, inclusive = true)
+            .build()
     )
 }
 
