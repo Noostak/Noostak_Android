8c98d84849cee42322f684fc9b1598eb9650eb8f
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 00:59:33 +0900

    #10 [FEAT] Group 페이지 구현

--- presentation/src/main/java/com/sopt/presentation/group/GroupRoute.kt
+++ presentation/src/main/java/com/sopt/presentation/group/GroupRoute.kt
@@ -1,36 +1,138 @@
 package com.sopt.presentation.group
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.flowWithLifecycle
+import com.sopt.core.designsystem.component.button.NoostakFloatingActionButtonWithText
+import com.sopt.core.designsystem.component.topappbar.BaseTopAppBar
+import com.sopt.core.designsystem.screen.NoostakEmptyScreen
 import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.domain.entity.GroupEntity
+import com.sopt.presentation.R
+import com.sopt.presentation.group.component.GroupFloatingActionButton
+import com.sopt.presentation.group.component.GroupItem
+import kotlinx.coroutines.flow.collectLatest
 
 @Composable
 fun GroupRoute(
-    paddingValues: PaddingValues
+    viewModel: GroupViewModel = hiltViewModel()
 ) {
-    GroupScreen(paddingValues = paddingValues)
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val groupItems = viewModel.groupItems
+
+    val isEmpty = groupItems.isEmpty()
+
+    LaunchedEffect(lifecycleOwner) {
+        viewModel.sideEffect.flowWithLifecycle(lifecycleOwner.lifecycle)
+            .collectLatest { sideEffect ->
+                when (sideEffect) {
+                    is GroupSideEffect.NavigateToDetail -> GroupSideEffect.NavigateToDetail(
+                        sideEffect.groupId
+                    )
+                }
+            }
+    }
+
+    when {
+        isEmpty -> NoostakEmptyScreen(
+            emptyText = R.string.text_group_empty,
+            color = NoostakTheme.colors.gray600,
+            style = NoostakTheme.typography.b4Regular
+        )
+
+        else -> GroupScreen(
+            groupItems = groupItems,
+            onItemClick = viewModel::navigateToDetail
+        )
+    }
 }
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun GroupScreen(
-    paddingValues: PaddingValues = PaddingValues()
+    groupItems: List<GroupEntity>,
+    onItemClick: (Long) -> Unit
 ) {
-    Column(
+    var isFabClicked by remember { mutableStateOf(false) }
+
+    Scaffold(
         modifier = Modifier
-            .fillMaxSize()
-            .padding(paddingValues),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Appointment Screen")
+            .statusBarsPadding()
+            .navigationBarsPadding(),
+        topBar = {
+            BaseTopAppBar(
+                title = stringResource(R.string.bottom_nav_group),
+                modifier = Modifier,
+                isBackButton = false
+            )
+        },
+        floatingActionButton = {
+            if (!isFabClicked) {
+                NoostakFloatingActionButtonWithText(
+                    title = stringResource(R.string.fab_group_create),
+                    modifier = Modifier.offset(x = 0.dp, y = (-74).dp)
+                ) {
+                    isFabClicked = true
+                }
+            } else {
+                GroupFloatingActionButton {
+                    isFabClicked = false
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.End,
+    ) { innerPadding ->
+        Box {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding)
+                    .padding(horizontal = 16.dp)
+            ) {
+                items(items = groupItems, key = { item -> item.groupId }) {
+                    GroupItem(it, onItemClick)
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = NoostakTheme.colors.gray100,
+                    )
+                }
+            }
+
+            if (isFabClicked) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Black.copy(alpha = 0.5f))
+                        .clickable { isFabClicked = false }
+                )
+            }
+        }
     }
 }
 
@@ -38,6 +140,18 @@ fun GroupScreen(
 @Composable
 fun GroupScreenPreview() {
     NoostakAndroidTheme {
-        GroupScreen()
+        GroupScreen(
+            groupItems = listOf(
+                GroupEntity(groupId = 1, groupName = "누스탁", groupPersonnel = 15, newsImage = null),
+                GroupEntity(
+                    groupId = 2,
+                    groupName = "유니보이스",
+                    groupPersonnel = 16,
+                    newsImage = null
+                ),
+                GroupEntity(groupId = 3, groupName = "솝트", groupPersonnel = 191, newsImage = null),
+            ),
+            onItemClick = {}
+        )
     }
 }
