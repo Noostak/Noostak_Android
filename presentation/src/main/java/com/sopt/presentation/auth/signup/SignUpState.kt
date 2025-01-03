9a3450c998a4a4abada49137e9838447aa927a8e
youjin09222 <youjin09222@naver.com>
2025-01-03 23:34:08 +0900

    #6 [REFACTOR] SignUpState 수정

--- presentation/src/main/java/com/sopt/presentation/auth/signup/SignUpState.kt
+++ presentation/src/main/java/com/sopt/presentation/auth/signup/SignUpState.kt
@@ -1,8 +1,7 @@
 package com.sopt.presentation.auth.signup
 data class SignUpState(
     val name: String = "",
-    val profileImage: String = "basic",
-    val isButtonValid: Boolean = false,
-    val authId: String = "",
-    val showBottomSheet: Boolean = false
+    val profileImage: String? = "",
+    val authType: String = "",
+    val authId: String = ""
 )
