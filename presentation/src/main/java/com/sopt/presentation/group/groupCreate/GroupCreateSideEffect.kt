fb3e3769c4ac8b536d59a7df691dfa6b866af3ae
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 03:41:46 +0900

    #10 [DEL] 불필요한 코드 제거

--- presentation/src/main/java/com/sopt/presentation/group/groupCreate/GroupCreateSideEffect.kt
+++ presentation/src/main/java/com/sopt/presentation/group/groupCreate/GroupCreateSideEffect.kt
@@ -2,8 +2,6 @@ package com.sopt.presentation.group.groupCreate
 
 sealed interface GroupCreateSideEffect {
     data object RequestImagePicker : GroupCreateSideEffect
-
     data object ShowPermissionDeniedDialog : GroupCreateSideEffect
-
     data object NavigateToGroupCreateSuccess : GroupCreateSideEffect
 }
