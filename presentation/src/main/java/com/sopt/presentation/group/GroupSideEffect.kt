ab17ba61c5bca2243913ae81ff1081b1ed09cf86
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 02:51:20 +0900

    #10 [FEAT] Group 페이지 SideEffect nav event 추가

--- presentation/src/main/java/com/sopt/presentation/group/GroupSideEffect.kt
+++ presentation/src/main/java/com/sopt/presentation/group/GroupSideEffect.kt
@@ -2,4 +2,6 @@ package com.sopt.presentation.group
 
 sealed interface GroupSideEffect {
     data class NavigateToGroupDetail(val groupId: Long) : GroupSideEffect
+    data object NavigateToGroupCreate : GroupSideEffect
+    data object NavigateToGroupEnter : GroupSideEffect
 }
