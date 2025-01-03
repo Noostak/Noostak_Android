e2c5954e0b35d8dd2e57566dbf193394224d5db3
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 01:00:36 +0900

    #10 [FEAT] GroupProfileEntity 구현

---
+++ domain/src/main/java/com/sopt/domain/entity/GroupProfileEntity.kt
@@ -0,0 +1,8 @@
+package com.sopt.domain.entity
+
+data class GroupProfileEntity(
+    val groupName: String = "",
+    val selectedImageUri: String? = null,
+    val isPermissionGranted: Boolean = false,
+    val isGroupNameCheck: Boolean = false,
+)
