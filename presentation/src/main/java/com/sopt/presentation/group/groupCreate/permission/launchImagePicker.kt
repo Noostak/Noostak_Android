290a0877a7cfafc32b9e2d9306d533271ab1ee32
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 01:04:58 +0900

    #10 [FEAT] launchImagePicker 구현

---
+++ presentation/src/main/java/com/sopt/presentation/group/groupCreate/permission/launchImagePicker.kt
@@ -0,0 +1,19 @@
+package com.sopt.presentation.group.groupCreate.permission
+
+import android.os.Build
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+
+fun launchImagePicker(
+    galleryLauncher: ActivityResultLauncher<String>,
+    photoPickerLauncher: ActivityResultLauncher<PickVisualMediaRequest>,
+) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+        // API 33 미만: 갤러리 실행
+        galleryLauncher.launch("image/*")
+    } else {
+        // API 33 이상: 포토피커 실행 (이미지만 선택)
+        photoPickerLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+    }
+}
