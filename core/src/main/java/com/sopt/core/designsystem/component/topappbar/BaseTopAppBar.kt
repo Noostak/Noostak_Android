20b6490b4f53321e3640ac18927a8950b74961e8
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 00:57:41 +0900

    #10 [MOD] BaseTopAppBar 커스텀 추가

--- core/src/main/java/com/sopt/core/designsystem/component/topappbar/BaseTopAppBar.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/topappbar/BaseTopAppBar.kt
@@ -1,7 +1,9 @@
 package com.sopt.core.designsystem.component.topappbar
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -13,15 +15,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.core.R
+import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseTopAppBar(
     title: String = "",
+    style: TextStyle = NoostakTheme.typography.b2Regular,
     modifier: Modifier,
+    isBackButton: Boolean = true,
     onBackButtonClick: () -> Unit = {}
 ) {
     CenterAlignedTopAppBar(
@@ -29,21 +37,26 @@ fun BaseTopAppBar(
         title = {
             Text(
                 text = title,
-                textAlign = TextAlign.Center
+                style = style,
+                textAlign = TextAlign.Center,
             )
         },
         navigationIcon = {
-            IconButton(
-                onClick = {
-                    onBackButtonClick()
+            if (isBackButton) {
+                IconButton(
+                    onClick = {
+                        onBackButtonClick()
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_back),
+                        contentDescription = stringResource(id = R.string.ic_back),
+                        modifier = Modifier
+                            .padding(start = 8.dp)
+                    )
                 }
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_back),
-                    contentDescription = stringResource(id = R.string.ic_back),
-                    modifier = Modifier
-                        .padding(start = 8.dp)
-                )
+            } else {
+                Spacer(modifier = Modifier.width(0.dp))
             }
         },
         colors = TopAppBarDefaults.topAppBarColors(White),
@@ -54,4 +67,12 @@ fun BaseTopAppBar(
             bottom = 0
         )
     )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BaseTopAppBarPreview() {
+    NoostakAndroidTheme {
+        BaseTopAppBar(title = "test", modifier = Modifier)
+    }
 }
