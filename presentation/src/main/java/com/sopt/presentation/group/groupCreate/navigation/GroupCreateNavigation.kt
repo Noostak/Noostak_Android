c4a215ad004ae47b44098179c5302bc0e2c5ca15
Eonji-sw <ejkim0625@gmail.com>
2025-01-04 02:48:46 +0900

    #10 [FEAT] GroupCreateNavigation 구현

---
+++ presentation/src/main/java/com/sopt/presentation/group/groupCreate/navigation/GroupCreateNavigation.kt
@@ -0,0 +1,50 @@
+package com.sopt.presentation.group.groupCreate.navigation
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.NavOptions
+import androidx.navigation.compose.composable
+import com.sopt.core.navigation.Route
+import com.sopt.presentation.group.groupCreate.GroupCreateRoute
+import com.sopt.presentation.group.groupCreateSuccess.navigation.navigateToGroupCreateSuccess
+import kotlinx.serialization.Serializable
+
+fun NavController.navigateToGroupCreate(navOptions: NavOptions? = null) {
+    navigate(
+        route = GroupCreate,
+        navOptions = navOptions
+    )
+}
+
+fun NavController.navigateToGroupEnter(navOptions: NavOptions? = null) {
+    navigate(
+        route = GroupEnter,
+        navOptions = navOptions
+    )
+}
+
+
+fun NavGraphBuilder.groupCreateNavGraph(
+    paddingValues: PaddingValues,
+    navHostController: NavController
+) {
+    composable<GroupCreate> {
+        GroupCreateRoute(
+            paddingValues = paddingValues,
+            navigateToGroupCreateSuccess = {
+                navHostController.navigateToGroupCreateSuccess()
+            }
+        )
+    }
+
+    composable<GroupEnter> {
+        // 이동 코드 추가하기
+    }
+}
+
+@Serializable
+data object GroupCreate : Route
+
+@Serializable
+data object GroupEnter : Route
