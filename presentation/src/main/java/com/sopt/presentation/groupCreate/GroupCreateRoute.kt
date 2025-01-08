d8adbd4bf7e025c54bcbaf8b28b30ab7b6c1bd34
Eonji-sw <ejkim0625@gmail.com>
2025-01-08 18:19:36 +0900

    #15 [MOD] NoostakTextField 적용

--- presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateRoute.kt
+++ presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateRoute.kt
@@ -36,10 +36,11 @@ import androidx.lifecycle.flowWithLifecycle
 import com.sopt.core.designsystem.component.button.NoostakBottomButton
 import com.sopt.core.designsystem.theme.NoostakAndroidTheme
 import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.core.type.TextFieldType
 import com.sopt.domain.entity.GroupProfileEntity
 import com.sopt.presentation.R
 import com.sopt.presentation.groupCreate.component.GroupProfileImagePicker
-import com.sopt.presentation.groupCreate.component.GroupProfileNameTextField
+import com.sopt.core.designsystem.component.textfield.NoostakTextField
 import com.sopt.presentation.groupCreate.permission.launchImagePicker
 import com.sopt.presentation.groupCreate.permission.rememberGalleryLauncher
 import com.sopt.presentation.groupCreate.permission.rememberPhotoPickerLauncher
@@ -169,9 +170,9 @@ fun GroupCreateScreen(
                     .align(Alignment.CenterHorizontally)
             )
             Spacer(modifier = Modifier.height(27.dp))
-            GroupProfileNameTextField(
+            NoostakTextField(
+                textFieldType = TextFieldType.GROUP,
                 value = groupProfileState.groupName,
-                placeholder = stringResource(R.string.tf_group_create_placeholder),
                 onValueChange = { onNameChange(it) }
             )
         }
