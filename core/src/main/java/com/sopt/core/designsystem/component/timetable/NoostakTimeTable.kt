726c1b4554fc2c9418ed92b9b1f353622019f6e8
gaeulzzang <gaeulzzang@gmail.com>
2025-01-09 14:21:46 +0900

    #8 [REMOVE] 불필요한 임포트 제거

--- core/src/main/java/com/sopt/core/designsystem/component/timetable/NoostakTimeTable.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/timetable/NoostakTimeTable.kt
@@ -2,12 +2,10 @@ package com.sopt.core.designsystem.component.timetable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,7 +13,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
