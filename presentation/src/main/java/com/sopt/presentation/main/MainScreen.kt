f4155211f3c6aed37668ed46ff446fbff0cf52c1
youjin09222 <youjin09222@naver.com>
2025-01-09 22:27:41 +0900

    #6 [REFACTOR] ktlint 적용

--- presentation/src/main/java/com/sopt/presentation/main/MainScreen.kt
+++ presentation/src/main/java/com/sopt/presentation/main/MainScreen.kt
@@ -38,11 +38,11 @@ import androidx.navigation.compose.NavHost
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import com.sopt.core.designsystem.component.snackbar.NoostakSnackBar
 import com.sopt.core.util.NoRippleInteractionSource
+import com.sopt.presentation.appointment.navigation.appointmentNavGraph
 import com.sopt.presentation.auth.login.navigation.loginNavGraph
 import com.sopt.presentation.auth.signup.checkInvite.navigation.checkInviteNavGraph
 import com.sopt.presentation.auth.signup.inputGroupCode.navigation.inputGroupCodeNavGraph
 import com.sopt.presentation.auth.signup.navigation.signUpNavGraph
-import com.sopt.presentation.appointment.navigation.appointmentNavGraph
 import com.sopt.presentation.calendar.navigation.calendarNavGraph
 import com.sopt.presentation.example.navigation.exampleNavGraph
 import com.sopt.presentation.group.navigation.groupNavGraph
