f9d1fbd7c8a0ff3ca60f17205953f4e417b64dc0
gaeulzzang <gaeulzzang@gmail.com>
2025-01-11 02:24:04 +0900

    #20 [UI] 약속 등록 확인 페이지 구현

--- core/src/main/java/com/sopt/core/designsystem/component/chip/NoostakCategoryChip.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/chip/NoostakCategoryChip.kt
@@ -9,13 +9,19 @@ import com.sopt.core.designsystem.theme.NoostakTheme
 @Composable
 fun NoostakCategoryChip(
     text: String,
-    backgroundColor: Color
+    backgroundColor: Color = NoostakTheme.colors.orange
 ) {
     NoostakChip(
         text = text,
         textStyle = NoostakTheme.typography.c2SemiBold,
         textColor = NoostakTheme.colors.white,
-        backgroundColor = backgroundColor,
+        backgroundColor = when(text) {
+            "중요" -> NoostakTheme.colors.orange
+            "일정" -> NoostakTheme.colors.blue
+            "취미" -> NoostakTheme.colors.purple
+            "기타" -> NoostakTheme.colors.mint
+            else -> backgroundColor
+        },
         borderColor = Color.Transparent,
         horizontalPaddingValues = dimensionResource(id = R.dimen.category_chip_horizontal_padding),
         verticalPaddingValues = dimensionResource(id = R.dimen.category_chip_vertical_padding)
