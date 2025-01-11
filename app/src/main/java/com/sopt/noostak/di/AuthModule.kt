a5e8114fac357def082df35d91411332326a2fc3
youjin09222 <youjin09222@naver.com>
2025-01-12 08:17:39 +0900

    #17 [ADD] GoogleWebClient ID 제공 메서드 추가

---
+++ app/src/main/java/com/sopt/noostak/di/AuthModule.kt
@@ -0,0 +1,20 @@
+package com.sopt.noostak.di
+
+import com.sopt.noostak.BuildConfig
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Named
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
+    @Provides
+    @Singleton
+    @Named("GoogleClientId")
+    fun provideGoogleClientId(): String {
+        return BuildConfig.GOOGLE_CLIENT_ID
+    }
+}
