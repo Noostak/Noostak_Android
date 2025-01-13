a7276cfa55f3e37c774332ee1d85f71de7ce177b
gaeulzzang <gaeulzzang@gmail.com>
2025-01-13 16:25:00 +0900

    #22 [REMOVE] 주석 제거

--- core/src/main/java/com/sopt/core/util/RearrangeList.kt
+++ core/src/main/java/com/sopt/core/util/RearrangeList.kt
@@ -8,12 +8,10 @@ class RearrangeList {
         data: List<String>
     ): List<String> {
         val membersList = data.toMutableList()
-        // Validate position is within bounds and compare with name
         if (identity.position in membersList.indices && membersList[identity.position] == identity.name) {
-            val myInfo = membersList.removeAt(identity.position) // Remove the element
-            membersList.add(0, myInfo) // Add it to the front
+            val myInfo = membersList.removeAt(identity.position)
+            membersList.add(0, myInfo)
         }
-
         return membersList
     }
 }
