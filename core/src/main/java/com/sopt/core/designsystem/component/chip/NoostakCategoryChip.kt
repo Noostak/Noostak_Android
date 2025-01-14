f464fbcc8a4bdb6143cccbcd02c1c0464b3a1422
gaeulzzang <gaeulzzang@gmail.com>
2025-01-14 19:55:37 +0900

    #20 [REFACTOR] CategoryType 스트링 추출

--- core/src/main/java/com/sopt/core/designsystem/component/chip/NoostakCategoryChip.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/chip/NoostakCategoryChip.kt
@@ -2,6 +2,7 @@ package com.sopt.core.designsystem.component.chip
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.dimensionResource
 import com.sopt.core.R
 import com.sopt.core.designsystem.theme.NoostakTheme
@@ -11,7 +12,8 @@ import com.sopt.core.type.CategoryType
 fun NoostakCategoryChip(
     text: String
 ) {
-    val categoryType = CategoryType.fromText(text)
+    val context = LocalContext.current
+    val categoryType = CategoryType.fromText(context, text)
     val resolvedBackgroundColor = when (categoryType) {
         CategoryType.IMPORTANT -> NoostakTheme.colors.orange
         CategoryType.SCHEDULE -> NoostakTheme.colors.blue
--- core/src/main/java/com/sopt/core/type/CategoryType.kt
+++ core/src/main/java/com/sopt/core/type/CategoryType.kt
@@ -1,15 +1,18 @@
 package com.sopt.core.type
 
+import android.content.Context
+import com.sopt.core.R
+
 enum class CategoryType {
     IMPORTANT, SCHEDULE, HOBBY, ETC;
 
     companion object {
-        fun fromText(text: String): CategoryType {
+        fun fromText(context: Context, text: String): CategoryType {
             return when (text.uppercase()) {
-                "중요" -> IMPORTANT
-                "일정" -> SCHEDULE
-                "취미" -> HOBBY
-                "기타" -> ETC
+                context.getString(R.string.category_chip_important) -> IMPORTANT
+                context.getString(R.string.category_chip_schedule) -> SCHEDULE
+                context.getString(R.string.category_chip_hobby) -> HOBBY
+                context.getString(R.string.category_chip_etc) -> ETC
                 else -> ETC
             }
         }
