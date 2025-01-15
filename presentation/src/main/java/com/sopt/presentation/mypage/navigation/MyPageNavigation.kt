b11e6264c1a1ec91049adb2c4b079b761506d983
Eonji-sw <ejkim0625@gmail.com>
2025-01-15 20:40:24 +0900

    #25 [MOD] 필요없는 코드 제거

--- presentation/src/main/java/com/sopt/presentation/mypage/navigation/MyPageNavigation.kt
+++ presentation/src/main/java/com/sopt/presentation/mypage/navigation/MyPageNavigation.kt
@@ -1,13 +1,11 @@
 package com.sopt.presentation.mypage.navigation
 
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
 import com.sopt.core.navigation.MainTabRoute
-import com.sopt.presentation.example.navigation.navigateExample
 import com.sopt.presentation.mypage.MyPageRoute
 import kotlinx.serialization.Serializable
 
@@ -19,16 +17,10 @@ fun NavController.navigateMyPage(navOptions: NavOptions? = null) {
 }
 
 fun NavGraphBuilder.myPageNavGraph(
-    paddingValues: PaddingValues,
     navHostController: NavHostController
 ) {
     composable<MyPage> {
-        MyPageRoute(
-            paddingValues = paddingValues,
-            navigateToExample = { text ->
-                navHostController.navigateExample(text = text)
-            }
-        )
+        MyPageRoute()
     }
 }
 
