fabd2a0daef7cf16e1cee53378e39993473bab04
youjin09222 <youjin09222@naver.com>
2025-01-16 05:28:26 +0900

    #17 [ADD] UserEntity 추가

---
+++ domain/src/main/java/com/sopt/domain/entity/UserEntity.kt
@@ -0,0 +1,10 @@
+package com.sopt.domain.entity
+
+data class UserEntity(
+    val accessToken: String?,
+    val refreshToken: String?,
+    val userId: Int?,
+    val isAutoLogin: Boolean,
+    val nickName: String,
+    val profileImage: String
+)
