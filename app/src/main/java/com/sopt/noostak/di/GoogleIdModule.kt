f98694d07255c645e485cbf844db41175450c1d5
youjin09222 <youjin09222@naver.com>
2025-01-16 05:50:33 +0900

    #17 [RENAME] AuthModule to GoogleIdModule

--- app/src/main/java/com/sopt/noostak/di/GoogleIdModule.kt
+++ app/src/main/java/com/sopt/noostak/di/GoogleIdModule.kt
@@ -10,7 +10,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object AuthModule {
+object GoogleIdModule {
     @Provides
     @Singleton
     @Named("GoogleClientId")
