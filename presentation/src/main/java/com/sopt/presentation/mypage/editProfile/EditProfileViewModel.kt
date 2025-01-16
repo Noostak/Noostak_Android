445f99e3dd2376e45e62815bf56b4c55d47181de
youjin09222 <youjin09222@naver.com>
2025-01-17 06:21:02 +0900

    #29 [FEAT] EditProfile에 사용자 정보 적용

--- presentation/src/main/java/com/sopt/presentation/mypage/editProfile/EditProfileViewModel.kt
+++ presentation/src/main/java/com/sopt/presentation/mypage/editProfile/EditProfileViewModel.kt
@@ -3,20 +3,46 @@ package com.sopt.presentation.mypage.editProfile
 import androidx.lifecycle.viewModelScope
 import com.sopt.core.util.BaseViewModel
 import com.sopt.domain.entity.UserEntity
+import com.sopt.domain.repository.UserInfoRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class EditProfileViewModel @Inject constructor() : BaseViewModel<EditProfileSideEffect>() {
+class EditProfileViewModel @Inject constructor(
+    private val userInfoRepository: UserInfoRepository
+) : BaseViewModel<EditProfileSideEffect>() {
+
     private val _editProfileState = MutableStateFlow(EditProfileState())
     val editProfileState: StateFlow<EditProfileState> = _editProfileState
 
     private val _userInfoState = MutableStateFlow(UserEntity())
     val userInfoState: StateFlow<UserEntity> = _userInfoState
+
+    init {
+        loadUserInfo()
+    }
+
+    private fun loadUserInfo() {
+        viewModelScope.launch {
+            loadNickName()
+            loadProfileImage()
+        }
+    }
+
+    private suspend fun loadNickName() {
+        val nickName = userInfoRepository.getNickName().first()
+        _userInfoState.update { it.copy(nickName = nickName) }
+    }
+
+    private suspend fun loadProfileImage() {
+        val profileImageUrl = userInfoRepository.getProfileImage().first()
+        _userInfoState.update { it.copy(profileImage = profileImageUrl) }
+    }
 
     fun navigateUp() {
         emitSideEffect(EditProfileSideEffect.NavigateUp)
