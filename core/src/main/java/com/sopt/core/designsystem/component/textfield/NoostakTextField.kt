28228657e0e7f5aa3da1a0261c7682878db3f47b
Eonji-sw <ejkim0625@gmail.com>
2025-01-24 02:56:24 +0900

    #37 [MOD] 불필요한 코드 제거

--- core/src/main/java/com/sopt/core/designsystem/component/textfield/NoostakTextField.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/textfield/NoostakTextField.kt
@@ -88,7 +88,7 @@ fun NoostakTextField(
                     textStyle = textStyle,
                     onValueChange = { newValue ->
                         if (textFieldType != TextFieldType.SIGNUP && textFieldType != TextFieldType.EDITPROFILE) {
-                            if (newValue.replace(" ", "").length <= maxLength) {
+                            if (newValue.length <= maxLength) {
                                 onValueChange(newValue)
                             }
                         } else {
