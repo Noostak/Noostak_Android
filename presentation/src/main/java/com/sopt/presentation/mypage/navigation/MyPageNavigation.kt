2f9495894abb2db4eb0f44b4bb1cf9aff3b8fcb9
Eonji-sw <ejkim0625@gmail.com>
2025-01-24 02:55:14 +0900

    #37 [FEAT] navToLogin 구현

--- presentation/src/main/java/com/sopt/presentation/mypage/navigation/MyPageNavigation.kt
+++ presentation/src/main/java/com/sopt/presentation/mypage/navigation/MyPageNavigation.kt
@@ -8,6 +8,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.sopt.core.navigation.MainTabRoute
 import com.sopt.core.navigation.Route
+import com.sopt.presentation.auth.login.navigation.navigateToLogin
 import com.sopt.presentation.mypage.MyPageRoute
 import com.sopt.presentation.mypage.editProfile.EditProfileRoute
 import kotlinx.serialization.Serializable
@@ -43,6 +44,10 @@ fun NavGraphBuilder.myPageNavGraph(
                     nickname = nickname,
                     profileImage = profileImage
                 )
+            },
+            navigateToLogin = {
+                navHostController.popBackStack()
+                navHostController.navigateToLogin()
             }
         )
     }
