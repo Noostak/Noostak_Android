4835290f2ad02f0758bb38c24fcecfa707d1bbd6
youjin09222 <youjin09222@naver.com>
2025-02-13 18:34:01 +0900

    #43 [FEAT] 요일 Enum WeekDayType 추가

---
+++ core/src/main/java/com/sopt/core/type/WeekDay.kt
@@ -0,0 +1,13 @@
+package com.sopt.core.type
+
+enum class WeekDayType(
+    val koreanDay: String,
+) {
+    SUNDAY(koreanDay = "일"),
+    MONDAY(koreanDay = "월"),
+    TUESDAY(koreanDay = "화"),
+    WEDNESDAY(koreanDay = "수"),
+    THURSDAY(koreanDay = "목"),
+    FRIDAY(koreanDay = "금"),
+    SATURDAY(koreanDay = "토");
+}
