7039fa55fab0012ac28404789b44c2b3bf468264
youjin09222 <youjin09222@naver.com>
2025-02-13 23:16:58 +0900

    #43 [FEAT] CalendarViewModel에 loadSampleSchedule 추가

--- presentation/src/main/java/com/sopt/presentation/calendar/CalendarViewModel.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/CalendarViewModel.kt
@@ -1,8 +1,46 @@
 package com.sopt.presentation.calendar
 
+import androidx.lifecycle.viewModelScope
 import com.sopt.core.util.BaseViewModel
+import com.sopt.domain.entity.CalendarSchedule
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class CalendarViewModel @Inject constructor() : BaseViewModel<CalendarSideEffect>()
+class CalendarViewModel @Inject constructor() : BaseViewModel<CalendarSideEffect>() {
+
+    private val _scheduleMap = MutableStateFlow<Map<String, List<CalendarSchedule>>>(emptyMap())
+    val scheduleMap: StateFlow<Map<String, List<CalendarSchedule>>> get() = _scheduleMap
+
+    init {
+        loadSampleSchedule()
+    }
+
+    private fun loadSampleSchedule() {
+        viewModelScope.launch {
+            _scheduleMap.value = mapOf(
+                "2025-02-04" to listOf(
+                    CalendarSchedule(title = "회의", color = "#A9DBBE"),
+                    CalendarSchedule(title = "책 읽기", color = "#8D78D8"),
+                    CalendarSchedule(title = "영화 관람", color = "#3E8EFF"),
+                    CalendarSchedule(title = "스터디 모임", color = "#3E8EFF"),
+                    CalendarSchedule(title = "저녁 식사", color = "#3E8EFF")
+                ),
+                "2025-02-12" to listOf(
+                    CalendarSchedule(title = "회의", color = "#A9DBBE"),
+                    CalendarSchedule(title = "운동", color = "#8D78D8")
+                ),
+                "2025-02-13" to listOf(
+                    CalendarSchedule(title = "회의", color = "#A9DBBE")
+                ),
+                "2025-02-14" to listOf(
+                    CalendarSchedule(title = "출근", color = "#3E8EFF"),
+                    CalendarSchedule(title = "이름이 긴 약속", color = "#8D78D8")
+                )
+            )
+        }
+    }
+}
