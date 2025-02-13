7503b3147211b9e8a8690a08e5180d2ae6abb5e4
youjin09222 <youjin09222@naver.com>
2025-02-13 18:31:24 +0900

    #43 [UI] YearMonthHeader 컴포넌트 구현

---
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/YearMonthHeader.kt
@@ -0,0 +1,48 @@
+package com.sopt.presentation.calendar.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.core.extension.toYearMonthString
+import java.time.YearMonth
+
+@Composable
+fun YearMonthHeader(
+    date: YearMonth,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(color = NoostakTheme.colors.white)
+    ) {
+        Row(
+            modifier = Modifier.align(Alignment.CenterStart),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = date.toYearMonthString(),
+                style = NoostakTheme.typography.b1SemiBold,
+                color = NoostakTheme.colors.gray900,
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun YearMonthHeaderPreview() {
+    NoostakAndroidTheme {
+        YearMonthHeader(
+            date = YearMonth.now(),
+        )
+    }
+}
