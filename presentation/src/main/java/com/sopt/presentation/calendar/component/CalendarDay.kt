c52298ff4bfb1b52d70b459fdeaed173cd71b1da
youjin09222 <youjin09222@naver.com>
2025-02-13 15:08:27 +0900

    #43 [UI] CalendarDay 컴포넌트 구현

---
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/CalendarDay.kt
@@ -0,0 +1,81 @@
+package com.sopt.presentation.calendar.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.presentation.calendar.model.DayModel
+import java.time.LocalDate
+
+@Composable
+fun CalendarDay(
+    dayModel: DayModel,
+    isToday: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    val colors = NoostakTheme.colors
+
+    val backgroundColor by rememberUpdatedState(
+        when {
+            isToday -> colors.gray900
+            else -> Color.Transparent
+        }
+    )
+
+    val textColor by rememberUpdatedState(
+        when {
+            isToday -> colors.white
+            dayModel.isOtherMonth -> colors.gray600
+            else -> colors.gray900
+        }
+    )
+
+    Box(
+        modifier = modifier
+            .size(20.dp)
+            .background(color = backgroundColor, shape = CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = dayModel.day.dayOfMonth.toString(),
+            color = textColor,
+            style = NoostakTheme.typography.c3Regular
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CalendarDayPreview() {
+    NoostakAndroidTheme {
+        Row {
+            // 다른 달 날짜
+            CalendarDay(
+                dayModel = DayModel(day = LocalDate.now().minusMonths(1), isOtherMonth = true),
+                isToday = false
+            )
+            // 현재 달 날짜
+            CalendarDay(
+                dayModel = DayModel(day = LocalDate.now(), isOtherMonth = false),
+                isToday = false
+            )
+            // 오늘 날짜
+            CalendarDay(
+                dayModel = DayModel(day = LocalDate.now(), isOtherMonth = false),
+                isToday = true
+            )
+        }
+    }
+}
