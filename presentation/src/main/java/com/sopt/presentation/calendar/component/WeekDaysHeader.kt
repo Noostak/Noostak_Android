ee50d20058ac91bd0e26bd9a3689f7b576d3b65c
youjin09222 <youjin09222@naver.com>
2025-02-13 18:35:49 +0900

    #43 [UI] WeekDaysHeader 컴포넌트 구현

---
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/WeekDaysHeader.kt
@@ -0,0 +1,52 @@
+package com.sopt.presentation.calendar.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.core.type.WeekDayType
+
+@Composable
+fun WeekDaysHeader(
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .background(Color.White),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        WeekDayType.entries.forEach { day ->
+            Text(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(
+                        horizontal = 14.dp,
+                        vertical = 9.dp
+                    ),
+                text = day.koreanDay,
+                style = NoostakTheme.typography.c3Regular,
+                color = NoostakTheme.colors.gray800,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WeekDaysHeaderPreview() {
+    NoostakAndroidTheme {
+        WeekDaysHeader()
+    }
+}
