80fde71a97f9956b7522407b2fbd864affc3f451
Eonji-sw <ejkim0625@gmail.com>
2025-02-21 04:31:49 +0900

    #48 [UI] NoostakEmpty UI align 적용

--- core/src/main/java/com/sopt/core/designsystem/screen/NoostakEmptyScreen.kt
+++ core/src/main/java/com/sopt/core/designsystem/screen/NoostakEmptyScreen.kt
@@ -2,7 +2,6 @@ package com.sopt.core.designsystem.screen
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,19 +10,35 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.core.R
+import com.sopt.core.designsystem.theme.NoostakAndroidTheme
+import com.sopt.core.designsystem.theme.NoostakTheme
 
 @Composable
 fun NoostakEmptyScreen(@StringRes emptyText: Int, color: Color, style: TextStyle) {
     Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .aspectRatio(1f)
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
             modifier = Modifier.align(Alignment.Center),
             text = stringResource(emptyText),
             color = color,
-            style = style
+            style = style,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoostakEmptyScreenPreview() {
+    NoostakAndroidTheme {
+        NoostakEmptyScreen(
+            emptyText = R.string.text_dialog_type_group_content,
+            color = NoostakTheme.colors.black,
+            style = NoostakTheme.typography.b4Regular
         )
     }
 }
