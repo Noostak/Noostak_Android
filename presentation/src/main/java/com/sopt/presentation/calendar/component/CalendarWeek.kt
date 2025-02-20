f6699c8772121eab6b4407d35e12cd379e5b1472
youjin09222 <youjin09222@naver.com>
2025-02-21 01:47:34 +0900

    #43 [UI] CalendarWeek 중복 패딩 제거

--- presentation/src/main/java/com/sopt/presentation/calendar/component/CalendarWeek.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/CalendarWeek.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -40,8 +39,7 @@ internal fun CalendarWeek(
             Column(
                 modifier = Modifier
                     .fillMaxHeight()
-                    .weight(1f)
-                    .padding(top = 4.dp),
+                    .weight(1f),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 CalendarDay(
