f4974614e3199eb48c7c245bb68e14dd963efc94
youjin09222 <youjin09222@naver.com>
2025-02-20 16:23:03 +0900

    #43 [REFACTOR] WeekDaysHeader 배경색 NoostakTheme 적용

--- presentation/src/main/java/com/sopt/presentation/calendar/component/WeekDaysHeader.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/WeekDaysHeader.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,7 +22,7 @@ fun WeekDaysHeader(
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .background(Color.White),
+            .background(NoostakTheme.colors.white),
         horizontalArrangement = Arrangement.spacedBy(8.dp)
     ) {
         WeekDayType.entries.forEach { day ->
