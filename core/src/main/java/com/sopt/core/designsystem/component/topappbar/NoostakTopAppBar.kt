00c3cbbdf9209b65f1025595b1f62e8fe7458d0e
Eonji-sw <ejkim0625@gmail.com>
2025-02-21 04:31:13 +0900

    #48 [UI] UI function 인자화

--- core/src/main/java/com/sopt/core/designsystem/component/topappbar/NoostakTopAppBar.kt
+++ core/src/main/java/com/sopt/core/designsystem/component/topappbar/NoostakTopAppBar.kt
@@ -1,5 +1,6 @@
 package com.sopt.core.designsystem.component.topappbar
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -11,12 +12,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.White
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.sopt.core.R
 import com.sopt.core.designsystem.theme.NoostakAndroidTheme
 import com.sopt.core.designsystem.theme.NoostakTheme
@@ -30,7 +32,9 @@ fun NoostakTopAppBar(
     style: TextStyle = NoostakTheme.typography.b2Regular,
     modifier: Modifier = Modifier,
     isIconVisible: Boolean = true,
-    onBackButtonClick: () -> Unit = {}
+    onBackButtonClick: () -> Unit = {},
+    @DrawableRes iconResource: Int = R.drawable.ic_back_24,
+    paddingStart: Dp = 8.dp
 ) {
     CenterAlignedTopAppBar(
         modifier = modifier.fillMaxWidth(),
@@ -44,10 +48,10 @@ fun NoostakTopAppBar(
         },
         navigationIcon = {
             Icon(
-                painter = painterResource(id = R.drawable.ic_back),
+                painter = painterResource(id = iconResource),
                 contentDescription = stringResource(id = R.string.ic_back),
                 modifier = Modifier
-                    .padding(start = dimensionResource(id = R.dimen.appbar_start_padding))
+                    .padding(start = paddingStart)
                     .noRippleClickable { onBackButtonClick() }
                     .showIf(isIconVisible),
                 tint = NoostakTheme.colors.black
