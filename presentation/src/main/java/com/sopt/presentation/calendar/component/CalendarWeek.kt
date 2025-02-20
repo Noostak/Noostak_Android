d5da342b6de9449d7828b76f0ceacc77d0b3ec34
youjin09222 <youjin09222@naver.com>
2025-02-21 01:54:01 +0900

    #43 [UI] Calendar 크기 고정

--- presentation/src/main/java/com/sopt/presentation/calendar/component/CalendarWeek.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/CalendarWeek.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -38,7 +37,7 @@ internal fun CalendarWeek(
 
             Column(
                 modifier = Modifier
-                    .fillMaxHeight()
+                    .height(84.dp)
                     .weight(1f),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
