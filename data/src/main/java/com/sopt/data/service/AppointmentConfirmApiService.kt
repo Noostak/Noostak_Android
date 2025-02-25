39c2fe2d2401c94ff2de73c55083078e6266bcba
gaeulzzang <gaeulzzang@gmail.com>
2025-02-25 21:40:29 +0900

    #53 [FEAT] 약속 옵션들 좋아요 service 작성

--- data/src/main/java/com/sopt/data/service/AppointmentConfirmApiService.kt
+++ data/src/main/java/com/sopt/data/service/AppointmentConfirmApiService.kt
@@ -2,6 +2,7 @@ package com.sopt.data.service
 
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.response.ResponseGetOptionsDto
+import com.sopt.data.dto.response.ResponseLikesDto
 import com.sopt.data.service.ApiKeyStorage.API
 import com.sopt.data.service.ApiKeyStorage.APPOINTMENTS
 import com.sopt.data.service.ApiKeyStorage.APPOINTMENT_ID
@@ -12,6 +13,7 @@ import com.sopt.data.service.ApiKeyStorage.OPTIONS
 import com.sopt.data.service.ApiKeyStorage.OPTION_ID
 import com.sopt.data.service.ApiKeyStorage.PROGRESS
 import com.sopt.data.service.ApiKeyStorage.V1
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -22,7 +24,14 @@ interface AppointmentConfirmApiService {
         @Path(GROUP_ID) groupId: Long,
         @Path(APPOINTMENT_ID) appointmentId: Long,
         @Path(OPTION_ID) optionId: Long
-    ): BaseResponse<Unit>
+    ): BaseResponse<ResponseLikesDto>
+
+    @DELETE("/$API/$V1/$GROUPS/{$GROUP_ID}/$APPOINTMENTS/{$APPOINTMENT_ID}/$PROGRESS/$OPTIONS/{$OPTION_ID}/$LIKES")
+    suspend fun deleteLike(
+        @Path(GROUP_ID) groupId: Long,
+        @Path(APPOINTMENT_ID) appointmentId: Long,
+        @Path(OPTION_ID) optionId: Long
+    ): BaseResponse<ResponseLikesDto>
 
     @GET("/$API/$V1/$APPOINTMENTS/{$APPOINTMENT_ID}/$OPTIONS")
     suspend fun getOptions(
