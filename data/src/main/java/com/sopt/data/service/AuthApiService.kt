6a45a6b48f38e78474e0dda0706d7b6efb340055
youjin09222 <youjin09222@naver.com>
2025-02-28 21:47:32 +0900

    #54 [ADD] AuthApiService에 postSocialLogin 추가

---
+++ data/src/main/java/com/sopt/data/service/AuthApiService.kt
@@ -0,0 +1,22 @@
+package com.sopt.data.service
+
+import com.sopt.data.dto.BaseResponse
+import com.sopt.data.dto.request.RequestPostSocialLoginDto
+import com.sopt.data.dto.response.ResponseSocialLoginDto
+import com.sopt.data.service.ApiKeyStorage.API
+import com.sopt.data.service.ApiKeyStorage.AUTH
+import com.sopt.data.service.ApiKeyStorage.AUTHORIZATION
+import com.sopt.data.service.ApiKeyStorage.SIGN_IN
+import com.sopt.data.service.ApiKeyStorage.V1
+import retrofit2.http.Body
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface AuthApiService {
+
+    @POST("/$API/$V1/$AUTH/$SIGN_IN")
+    suspend fun postSocialLogin(
+        @Header(AUTHORIZATION) token: String,
+        @Body requestPostSocialLoginDto: RequestPostSocialLoginDto
+    ): BaseResponse<ResponseSocialLoginDto>
+}
