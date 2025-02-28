127237a73b4aaeb38b7cfcea5a5fc6baa982920f
Eonji-sw <ejkim0625@gmail.com>
2025-02-28 17:34:49 +0900

    #48 [UI] CategoryType Color 확장함수 적용

--- presentation/src/main/java/com/sopt/presentation/calendar/component/bottomsheet/ScheduleColorChip.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/component/bottomsheet/ScheduleColorChip.kt
@@ -9,7 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.core.extension.toColor
 import com.sopt.core.type.CategoryType
 
 @Composable
@@ -21,12 +21,9 @@ fun ScheduleColorChip(category: String) {
             .size(width = 6.dp, height = 38.dp)
             .clip(RoundedCornerShape(31.dp))
             .background(
-                color = when (CategoryType.fromText(context, category)) {
-                    CategoryType.IMPORTANT -> NoostakTheme.colors.orange
-                    CategoryType.SCHEDULE -> NoostakTheme.colors.blue
-                    CategoryType.HOBBY -> NoostakTheme.colors.purple
-                    CategoryType.ETC -> NoostakTheme.colors.mint
-                }
+                color = CategoryType
+                    .fromText(context, category)
+                    .toColor()
             )
     )
 }
