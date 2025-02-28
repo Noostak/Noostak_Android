b7716b8408f2098c6a7f4410b339f9dc7353f749
youjin09222 <youjin09222@naver.com>
2025-02-28 22:42:51 +0900

    #54 [FEAT] AuthRepositoryImpl에 postReissueToken 구현

--- data/src/main/java/com/sopt/data/repositoryimpl/AuthRepositoryImpl.kt
+++ data/src/main/java/com/sopt/data/repositoryimpl/AuthRepositoryImpl.kt
@@ -2,9 +2,11 @@ package com.sopt.data.repositoryimpl
 
 import com.sopt.data.datasource.AuthDataSource
 import com.sopt.data.dto.request.RequestPostSocialLoginDto
+import com.sopt.data.mapper.toReissueTokenEntity
 import com.sopt.data.mapper.toUserEntity
 import com.sopt.domain.entity.AuthEntity
 import com.sopt.domain.entity.AuthTypeEntity
+import com.sopt.domain.entity.ReissueTokenEntity
 import com.sopt.domain.repository.AuthRepository
 import javax.inject.Inject
 
@@ -17,10 +19,19 @@ class AuthRepositoryImpl @Inject constructor(
         request: AuthTypeEntity
     ): Result<AuthEntity> {
         return runCatching {
-            val response =
-                authDataSource.postSocialLogin(token, RequestPostSocialLoginDto(request.authType))
+            val response = authDataSource.postSocialLogin(token, RequestPostSocialLoginDto(request.authType))
             response.result?.toUserEntity()
                 ?: throw Exception("postSocialLogin failed")
+        }
+    }
+
+    override suspend fun postReissueToken(
+        refreshToken: String
+    ): Result<ReissueTokenEntity> {
+        return runCatching {
+            val response = authDataSource.postReissueToken(refreshToken)
+            response.result?.toReissueTokenEntity()
+                ?: throw Exception("postReissueToken failed")
         }
     }
 }
