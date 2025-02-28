cd4b018ea7b96ad73ddf32fb67f56fc01221123e
youjin09222 <youjin09222@naver.com>
2025-02-28 22:40:04 +0900

    #54 [FEAT] AuthDataSourceImpl에 postReissueToken 구현

--- data/src/main/java/com/sopt/data/datasourceimpl/AuthDataSourceImpl.kt
+++ data/src/main/java/com/sopt/data/datasourceimpl/AuthDataSourceImpl.kt
@@ -3,6 +3,7 @@ package com.sopt.data.datasourceimpl
 import com.sopt.data.datasource.AuthDataSource
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.request.RequestPostSocialLoginDto
+import com.sopt.data.dto.response.ResponseReissueTokenDto
 import com.sopt.data.dto.response.ResponseSocialLoginDto
 import com.sopt.data.service.AuthApiService
 import javax.inject.Inject
@@ -16,5 +17,11 @@ class AuthDataSourceImpl @Inject constructor(
         request: RequestPostSocialLoginDto
     ): BaseResponse<ResponseSocialLoginDto> {
         return authApiService.postSocialLogin(token, request)
+    }
+
+    override suspend fun postReissueToken(
+        refreshToken: String
+    ): BaseResponse<ResponseReissueTokenDto> {
+        return authApiService.postReissueToken(refreshToken)
     }
 }
