5d7d91910c6b9c713823ad5a5a8a4e9aeaf591fd
youjin09222 <youjin09222@naver.com>
2025-02-28 22:42:00 +0900

    #54 [FEAT] AuthRepository에 postReissueToken 추가

--- domain/src/main/java/com/sopt/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/sopt/domain/repository/AuthRepository.kt
@@ -2,10 +2,16 @@ package com.sopt.domain.repository
 
 import com.sopt.domain.entity.AuthEntity
 import com.sopt.domain.entity.AuthTypeEntity
+import com.sopt.domain.entity.ReissueTokenEntity
 
 interface AuthRepository {
     suspend fun postSocialLogin(
         token: String,
         request: AuthTypeEntity
     ): Result<AuthEntity>
+
+    suspend fun postReissueToken(
+        refreshToken: String
+    ): Result<ReissueTokenEntity>
+
 }
