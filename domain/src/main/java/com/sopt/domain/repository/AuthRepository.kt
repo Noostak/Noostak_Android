05954c1180764560efbb7cc06dbc55b7f03a012c
youjin09222 <youjin09222@naver.com>
2025-02-28 23:10:29 +0900

    #54 [FEAT] AuthRepository에 postSignUp 추가

--- domain/src/main/java/com/sopt/domain/repository/AuthRepository.kt
+++ domain/src/main/java/com/sopt/domain/repository/AuthRepository.kt
@@ -4,6 +4,8 @@ import com.sopt.domain.entity.AuthEntity
 import com.sopt.domain.entity.AuthTypeEntity
 import com.sopt.domain.entity.RefreshTokenEntity
 import com.sopt.domain.entity.ReissueTokenEntity
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface AuthRepository {
     suspend fun postSocialLogin(
@@ -19,4 +21,11 @@ interface AuthRepository {
         code: String,
         authType: String
     ): Result<RefreshTokenEntity>
+
+    suspend fun postSignUp(
+        memberName: RequestBody,
+        memberProfileImage: MultipartBody.Part?,
+        authType: RequestBody,
+        authId: RequestBody
+    ): Result<AuthEntity>
 }
