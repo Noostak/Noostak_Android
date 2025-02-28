d498d7cbe82e20445be35f795d422a7db40c3f9e
youjin09222 <youjin09222@naver.com>
2025-02-28 23:00:27 +0900

    #54 [ADD] AuthApiService에 postSignUp 추가

--- data/src/main/java/com/sopt/data/service/AuthApiService.kt
+++ data/src/main/java/com/sopt/data/service/AuthApiService.kt
@@ -5,17 +5,23 @@ import com.sopt.data.dto.request.RequestPostSocialLoginDto
 import com.sopt.data.dto.request.RequestRefreshTokenDto
 import com.sopt.data.dto.response.ResponseRefreshTokenDto
 import com.sopt.data.dto.response.ResponseReissueTokenDto
+import com.sopt.data.dto.response.ResponseSignUpDto
 import com.sopt.data.dto.response.ResponseSocialLoginDto
 import com.sopt.data.service.ApiKeyStorage.API
 import com.sopt.data.service.ApiKeyStorage.AUTH
 import com.sopt.data.service.ApiKeyStorage.AUTHORIZATION
 import com.sopt.data.service.ApiKeyStorage.AUTHORIZE
 import com.sopt.data.service.ApiKeyStorage.SIGN_IN
+import com.sopt.data.service.ApiKeyStorage.SIGN_UP
 import com.sopt.data.service.ApiKeyStorage.TOKEN_REISSUE
 import com.sopt.data.service.ApiKeyStorage.V1
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import retrofit2.http.Body
 import retrofit2.http.Header
+import retrofit2.http.Multipart
 import retrofit2.http.POST
+import retrofit2.http.Part
 
 interface AuthApiService {
 
@@ -34,4 +40,13 @@ interface AuthApiService {
     suspend fun postRefreshToken(
         @Body request: RequestRefreshTokenDto
     ): BaseResponse<ResponseRefreshTokenDto>
+
+    @Multipart
+    @POST("/$API/$V1/$AUTH/$SIGN_UP")
+    suspend fun postSignUp(
+        @Part("memberName") memberName: RequestBody,
+        @Part memberProfileImage: MultipartBody.Part?,
+        @Part("authType") authType: RequestBody,
+        @Part("authId") authId: RequestBody
+    ): BaseResponse<ResponseSignUpDto>
 }
