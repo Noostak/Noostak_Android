453c927fc8c74aa51eb47c87b897b1e05112ddf0
youjin09222 <youjin09222@naver.com>
2025-02-28 22:35:24 +0900

    #54 [FEAT] AuthApiService에 postReissueToken 추가

--- data/src/main/java/com/sopt/data/service/AuthApiService.kt
+++ data/src/main/java/com/sopt/data/service/AuthApiService.kt
@@ -2,11 +2,13 @@ package com.sopt.data.service
 
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.request.RequestPostSocialLoginDto
+import com.sopt.data.dto.response.ResponseReissueTokenDto
 import com.sopt.data.dto.response.ResponseSocialLoginDto
 import com.sopt.data.service.ApiKeyStorage.API
 import com.sopt.data.service.ApiKeyStorage.AUTH
 import com.sopt.data.service.ApiKeyStorage.AUTHORIZATION
 import com.sopt.data.service.ApiKeyStorage.SIGN_IN
+import com.sopt.data.service.ApiKeyStorage.TOKEN_REISSUE
 import com.sopt.data.service.ApiKeyStorage.V1
 import retrofit2.http.Body
 import retrofit2.http.Header
@@ -19,4 +21,9 @@ interface AuthApiService {
         @Header(AUTHORIZATION) token: String,
         @Body requestPostSocialLoginDto: RequestPostSocialLoginDto
     ): BaseResponse<ResponseSocialLoginDto>
+
+    @POST("/$API/$V1/$AUTH/$TOKEN_REISSUE")
+    suspend fun postReissueToken(
+        @Header(AUTHORIZATION) refreshToken: String
+    ): BaseResponse<ResponseReissueTokenDto>
 }
