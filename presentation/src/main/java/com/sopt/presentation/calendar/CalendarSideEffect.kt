aa918d0a441a3ce6d1ea3aeb3a3415fa8dc6666f
Eonji-sw <ejkim0625@gmail.com>
2025-03-07 21:03:52 +0900

    #62 [MOD] NavigateToAppointmentCreate 구현

--- presentation/src/main/java/com/sopt/presentation/calendar/CalendarSideEffect.kt
+++ presentation/src/main/java/com/sopt/presentation/calendar/CalendarSideEffect.kt
@@ -3,6 +3,7 @@ package com.sopt.presentation.calendar
 sealed interface CalendarSideEffect {
     data object NavigateToGroupCreate : CalendarSideEffect
     data object NavigateToGroupEnter : CalendarSideEffect
+    data object NavigateToAppointmentCreate : CalendarSideEffect
     data class ShowAddDialog(val show: Boolean) : CalendarSideEffect
     data object ShowBottomSheet : CalendarSideEffect
 }
