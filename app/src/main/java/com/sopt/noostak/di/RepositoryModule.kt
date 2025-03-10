feaef91dcfb9d495d8c3eb3a0dd32ed0fb2e4bdd
youjin09222 <youjin09222@naver.com>
2025-03-11 06:53:17 +0900

    #54 [ADD] RepositoryModule에 AccountRepository 바인딩 추가

--- app/src/main/java/com/sopt/noostak/di/RepositoryModule.kt
+++ app/src/main/java/com/sopt/noostak/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.sopt.noostak.di
 
+import com.sopt.data.repositoryimpl.AccountRepositoryImpl
 import com.sopt.data.repositoryimpl.AuthRepositoryImpl
 import com.sopt.data.repositoryimpl.ExampleRepositoryImpl
 import com.sopt.data.repositoryimpl.UserInfoRepositoryImpl
+import com.sopt.domain.repository.AccountRepository
 import com.sopt.domain.repository.AuthRepository
 import com.sopt.domain.repository.ExampleRepository
 import com.sopt.domain.repository.UserInfoRepository
@@ -27,4 +29,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindAccountRepository(accountRepositoryImpl: AccountRepositoryImpl): AccountRepository
 }
