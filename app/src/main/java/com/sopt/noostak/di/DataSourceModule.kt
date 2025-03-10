b7aa0a0b5967afd293cf3b11a90f6715a725f5f0
youjin09222 <youjin09222@naver.com>
2025-03-11 06:51:12 +0900

    #54 [ADD] DataSourceModule에 AccountDataSource 바인딩 추가

--- app/src/main/java/com/sopt/noostak/di/DataSourceModule.kt
+++ app/src/main/java/com/sopt/noostak/di/DataSourceModule.kt
@@ -1,8 +1,10 @@
 package com.sopt.noostak.di
 
+import com.sopt.data.datasource.AccountDataSource
 import com.sopt.data.datasource.AuthDataSource
 import com.sopt.data.datasource.ExampleDataSource
 import com.sopt.data.datasource.UserDataSource
+import com.sopt.data.datasourceimpl.AccountDataSourceImpl
 import com.sopt.data.datasourceimpl.AuthDataSourceImpl
 import com.sopt.data.datasourceimpl.ExampleDataSourceImpl
 import com.sopt.data.datasourceimpl.UserDataSourceImpl
@@ -27,4 +29,8 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindAuthDataSource(authDataSourceImpl: AuthDataSourceImpl): AuthDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindAccountDataSource(accountDataSource: AccountDataSourceImpl): AccountDataSource
 }
