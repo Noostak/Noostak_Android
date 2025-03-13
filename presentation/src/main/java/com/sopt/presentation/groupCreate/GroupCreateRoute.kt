82c4f28a462a76fcabc55ecb4f9f6492eb64ef18
youjin09222 <youjin09222@naver.com>
2025-03-14 00:09:51 +0900

    #63 [REFACTOR] 그룹 생성에 기본 이미지 적용

--- presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateRoute.kt
+++ presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateRoute.kt
@@ -49,6 +49,7 @@ import com.sopt.core.designsystem.theme.NoostakAndroidTheme
 import com.sopt.core.designsystem.theme.NoostakTheme
 import com.sopt.core.extension.launchImagePicker
 import com.sopt.core.type.DialogType
+import com.sopt.core.type.ProfileType
 import com.sopt.core.type.TextFieldType
 import com.sopt.core.util.permission.ImagePickerLaunchers
 import com.sopt.domain.entity.GroupProfileEntity
@@ -222,6 +223,7 @@ fun GroupCreateScreen(
                 modifier = Modifier.padding(top = 70.dp)
             )
             ProfileImagePicker(
+                profileType = ProfileType.GROUP,
                 selectedImageUri = groupProfileState.selectedImageUri,
                 onCameraBtnClick = onProfileCameraBtnClick,
                 modifier = Modifier
