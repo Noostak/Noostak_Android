ccd511b9864f054926b668794f56271324d1630d
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 03:45:13 +0900

    #68 [MOD] GroupCreateSuccess groupInviteCode 인자 추가

--- presentation/src/main/java/com/sopt/presentation/groupCreate/groupCreateSuccess/navigation/GroupCreateSuccessNavigation.kt
+++ presentation/src/main/java/com/sopt/presentation/groupCreate/groupCreateSuccess/navigation/GroupCreateSuccessNavigation.kt
@@ -4,15 +4,19 @@ import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavOptions
 import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
 import com.sopt.core.navigation.Route
 import com.sopt.presentation.groupCreate.groupCreateSuccess.GroupCreateSuccessRoute
 import com.sopt.presentation.groupCreate.navigation.GroupCreate
 import com.sopt.presentation.groupDetail.navigation.navigateGroupDetail
 import kotlinx.serialization.Serializable
 
-fun NavController.navigateToGroupCreateSuccess(navOptions: NavOptions? = null) {
+fun NavController.navigateToGroupCreateSuccess(
+    groupInviteCode: String,
+    navOptions: NavOptions? = null
+) {
     navigate(
-        route = GroupCreateSuccess,
+        route = GroupCreateSuccess(groupInviteCode = groupInviteCode),
         navOptions = navOptions ?: NavOptions.Builder()
             .setPopUpTo(GroupCreate, inclusive = true)
             .build()
@@ -23,7 +27,9 @@ fun NavGraphBuilder.groupCreateSuccessNavGraph(
     navHostController: NavController
 ) {
     composable<GroupCreateSuccess> {
+        val args = it.toRoute<GroupCreateSuccess>()
         GroupCreateSuccessRoute(
+            groupInviteCode = args.groupInviteCode,
             navigateToGroupDetail = { groupId ->
                 navHostController.popBackStack()
                 navHostController.navigateGroupDetail(groupId = groupId)
@@ -33,4 +39,4 @@ fun NavGraphBuilder.groupCreateSuccessNavGraph(
 }
 
 @Serializable
-data object GroupCreateSuccess : Route
+data class GroupCreateSuccess(val groupInviteCode: String) : Route
