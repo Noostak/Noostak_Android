987433d42a37c2109e3fbc84422379d5832bdf00
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 03:37:30 +0900

    #68 [DEL] 불필요한 파일 제거

--- presentation/src/main/java/com/sopt/presentation/groupCreate/groupCreateSuccess/regex/Regex.kt
+++
@@ -1,8 +0,0 @@
-package com.sopt.presentation.groupCreate.groupCreateSuccess.regex
-
-class Regex {
-    fun generateRandomCode(): String {
-        val charset = ('0'..'9') + ('A'..'Z')
-        return List(6) { charset.random() }.joinToString("")
-    }
-}
