6c916e02499b6ec4f1766eecaeb75b6a2a984628
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 03:40:01 +0900

    #68 [FEAT] GroupDataSource 구현 - getGroups(), postGroup()

---
+++ data/src/main/java/com/sopt/data/datasource/GroupDataSource.kt
@@ -0,0 +1,15 @@
+package com.sopt.data.datasource
+
+import com.sopt.data.dto.BaseResponse
+import com.sopt.data.dto.response.ResponseGetGroupsDto
+import com.sopt.data.dto.response.ResponsePostGroupDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+
+interface GroupDataSource {
+    suspend fun getGroups(): BaseResponse<ResponseGetGroupsDto>
+    suspend fun postGroup(
+        groupName: RequestBody,
+        groupProfileImageUrl: MultipartBody.Part?
+    ): BaseResponse<ResponsePostGroupDto>
+}
