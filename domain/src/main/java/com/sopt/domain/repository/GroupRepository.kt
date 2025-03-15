a60e0c20d77b9bc18f5499b7c1b01665fe848043
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 04:37:08 +0900

    #68 [MOD] postGroup() 반환타입 GroupSuccessEntity 변경

--- domain/src/main/java/com/sopt/domain/repository/GroupRepository.kt
+++ domain/src/main/java/com/sopt/domain/repository/GroupRepository.kt
@@ -1,8 +1,12 @@
 package com.sopt.domain.repository
 
 import com.sopt.domain.entity.GroupEntity
+import com.sopt.domain.entity.GroupSuccessEntity
 
 interface GroupRepository {
     suspend fun getGroups(): Result<List<GroupEntity>>
-    suspend fun postGroup(groupName: String, groupProfileImageUrl: String?): Result<String>
+    suspend fun postGroup(
+        groupName: String,
+        groupProfileImageUrl: String?
+    ): Result<GroupSuccessEntity>
 }
