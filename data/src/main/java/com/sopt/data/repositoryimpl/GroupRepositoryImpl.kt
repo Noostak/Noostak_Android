8f6f93bfa0603ea2b142a79b3ae78c762d06d6df
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 03:40:55 +0900

    #68 [FEAT] GroupRepositoryImpl 구현 - getGroups(), postGroup()

---
+++ data/src/main/java/com/sopt/data/repositoryimpl/GroupRepositoryImpl.kt
@@ -0,0 +1,49 @@
+package com.sopt.data.repositoryimpl
+
+import android.content.ContentResolver
+import com.sopt.data.datasource.GroupDataSource
+import com.sopt.data.mapper.toGroupEntity
+import com.sopt.data.util.createImagePart
+import com.sopt.data.util.handleThrowable
+import com.sopt.domain.entity.GroupEntity
+import com.sopt.domain.repository.GroupRepository
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import org.json.JSONObject
+import javax.inject.Inject
+
+class GroupRepositoryImpl @Inject constructor(
+    private val contentResolver: ContentResolver,
+    private val groupDataSource: GroupDataSource
+) : GroupRepository {
+    override suspend fun getGroups(): Result<List<GroupEntity>> {
+        return runCatching {
+            groupDataSource.getGroups().result?.groups?.map { it.toGroupEntity() } ?: emptyList()
+        }
+    }
+
+    override suspend fun postGroup(
+        groupName: String,
+        groupProfileImageUrl: String?
+    ): Result<String> {
+        return runCatching {
+            val textRequestBody = createContentRequestBody(groupName)
+            val imagePart = contentResolver.createImagePart(groupProfileImageUrl, FILE_NAME)
+
+            groupDataSource.postGroup(textRequestBody, imagePart).result?.groupInviteCode ?: ""
+        }.onFailure { return it.handleThrowable() }
+    }
+
+    private fun createContentRequestBody(groupName: String): RequestBody {
+        val contentJson = JSONObject().apply {
+            put(GROUP_NAME, groupName)
+        }.toString()
+        return contentJson.toRequestBody("application/json".toMediaTypeOrNull())
+    }
+
+    companion object {
+        private const val FILE_NAME = "groupProfileImageUrl"
+        private const val GROUP_NAME = "groupName"
+    }
+}
