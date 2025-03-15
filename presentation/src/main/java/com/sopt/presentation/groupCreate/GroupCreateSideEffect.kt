fba5a4567a36c98b95708ed63279054b423372ab
Eonji-sw <ejkim0625@gmail.com>
2025-03-16 03:49:27 +0900

    #68 [MOD] GroupCreateSideEffect ShowErrorDialog 추가

--- presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateSideEffect.kt
+++ presentation/src/main/java/com/sopt/presentation/groupCreate/GroupCreateSideEffect.kt
@@ -3,6 +3,6 @@ package com.sopt.presentation.groupCreate
 sealed interface GroupCreateSideEffect {
     data object RequestImagePicker : GroupCreateSideEffect
     data object ShowSnackBar : GroupCreateSideEffect
-    data object ShowDialog : GroupCreateSideEffect
-    data object NavigateToGroupCreateSuccess : GroupCreateSideEffect
+    data object ShowErrorDialog : GroupCreateSideEffect
+    data class NavigateToGroupCreateSuccess(val groupInviteCode: String) : GroupCreateSideEffect
 }
