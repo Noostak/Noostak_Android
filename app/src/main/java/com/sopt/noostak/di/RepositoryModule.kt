f60559653a9f97d999f9785b4d522971eb2bf22e
youjin09222 <youjin09222@naver.com>
2025-03-21 20:22:45 +0900

    #58 [FEAT] RepositoryModule에 CalendarRepository 바인딩 추가

--- app/src/main/java/com/sopt/noostak/di/RepositoryModule.kt
+++ app/src/main/java/com/sopt/noostak/di/RepositoryModule.kt
@@ -3,11 +3,13 @@ package com.sopt.noostak.di
 import com.sopt.data.repositoryimpl.AccountRepositoryImpl
 import com.sopt.data.repositoryimpl.AppointmentConfirmRepositoryImpl
 import com.sopt.data.repositoryimpl.AuthRepositoryImpl
+import com.sopt.data.repositoryimpl.CalendarRepositoryImpl
 import com.sopt.data.repositoryimpl.ExampleRepositoryImpl
 import com.sopt.data.repositoryimpl.UserInfoRepositoryImpl
 import com.sopt.domain.repository.AccountRepository
 import com.sopt.domain.repository.AppointmentConfirmRepository
 import com.sopt.domain.repository.AuthRepository
+import com.sopt.domain.repository.CalendarRepository
 import com.sopt.domain.repository.ExampleRepository
 import com.sopt.domain.repository.UserInfoRepository
 import dagger.Binds
@@ -39,4 +41,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindAppointmentConfirmRepository(appointmentConfirmRepositoryImpl: AppointmentConfirmRepositoryImpl): AppointmentConfirmRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindCalendarRepository(calendarRepositoryImpl: CalendarRepositoryImpl): CalendarRepository
 }
