159682e045ed0ca6e68381e05304c29ea59838bc
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 02:58:57 +0900

    #72 [FEAT] ErrorDialog, FailureSnackBar SideEffect 추가

--- presentation/src/main/java/com/sopt/presentation/groupEnter/GroupEnterSideEffect.kt
+++ presentation/src/main/java/com/sopt/presentation/groupEnter/GroupEnterSideEffect.kt
@@ -3,4 +3,6 @@ package com.sopt.presentation.groupEnter
 sealed class GroupEnterSideEffect {
     data object NavigateUp : GroupEnterSideEffect()
     data object NavigateToGroup : GroupEnterSideEffect()
+    data object ShowErrorDialog : GroupEnterSideEffect()
+    data class ShowSnackBar(val message: Int) : GroupEnterSideEffect()
 }
