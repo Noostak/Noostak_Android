aa1c682228bbf2749586282fc2671dc795e7dd69
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 02:55:27 +0900

    #72 [FEAT] postGroupCode ApiService 구현

--- data/src/main/java/com/sopt/data/service/GroupApiService.kt
+++ data/src/main/java/com/sopt/data/service/GroupApiService.kt
@@ -2,12 +2,15 @@ package com.sopt.data.service
 
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.response.ResponseGetGroupsDto
+import com.sopt.data.dto.response.ResponsePostGroupCodeDto
 import com.sopt.data.dto.response.ResponsePostGroupDto
 import com.sopt.data.service.ApiKeyStorage.API
 import com.sopt.data.service.ApiKeyStorage.GROUPS
+import com.sopt.data.service.ApiKeyStorage.JOIN
 import com.sopt.data.service.ApiKeyStorage.V1
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Multipart
 import retrofit2.http.POST
@@ -23,4 +26,7 @@ interface GroupApiService {
         @Part("groupName") groupName: RequestBody,
         @Part groupProfileImage: MultipartBody.Part?
     ): BaseResponse<ResponsePostGroupDto>
+
+    @POST("/$API/$V1/$GROUPS/$JOIN")
+    suspend fun postGroupCode(@Body groupInviteCode: String): BaseResponse<ResponsePostGroupCodeDto>
 }
