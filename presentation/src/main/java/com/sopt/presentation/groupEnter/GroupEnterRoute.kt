f9b832beacef40a1413926dbff35dd636164cf42
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 02:58:41 +0900

    #72 [FEAT] postGroupCode 적용 및 ErrorDialog, FailureSnackBar 적용

--- presentation/src/main/java/com/sopt/presentation/groupEnter/GroupEnterRoute.kt
+++ presentation/src/main/java/com/sopt/presentation/groupEnter/GroupEnterRoute.kt
@@ -1,5 +1,8 @@
 package com.sopt.presentation.groupEnter
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -9,16 +12,21 @@ import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -26,12 +34,20 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.sopt.core.designsystem.component.button.NoostakBottomButton
+import com.sopt.core.designsystem.component.dialog.NoostakDialog
+import com.sopt.core.designsystem.component.snackbar.NoostakSnackBar
+import com.sopt.core.designsystem.component.snackbar.SNACK_BAR_DURATION
 import com.sopt.core.designsystem.component.textfield.OtpInputField
 import com.sopt.core.designsystem.component.topappbar.NoostakCloseAppBar
 import com.sopt.core.designsystem.theme.NoostakAndroidTheme
 import com.sopt.core.designsystem.theme.NoostakTheme
+import com.sopt.core.state.UiState
+import com.sopt.core.type.DialogType
 import com.sopt.presentation.R
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @Composable
 fun GroupEnterRoute(
@@ -39,25 +55,73 @@ fun GroupEnterRoute(
     navigateUp: () -> Unit,
     groupEnterViewModel: GroupEnterViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+
+    val showErrorDialog by groupEnterViewModel.showErrorDialog.collectAsStateWithLifecycle()
+
+    val groupEnterState by groupEnterViewModel.postGroupEnterState.collectAsStateWithLifecycle()
+
+    var groupCode by remember { mutableStateOf("") }
+
+    val snackBarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+    val snackBarVisible = remember { mutableStateOf(false) }
+
+    val onShowFailureSnackBar: (message: String) -> Unit = {
+        coroutineScope.launch {
+            snackBarVisible.value = true
+            val job = launch { snackBarHostState.showSnackbar(message = it) }
+            delay(SNACK_BAR_DURATION)
+            job.cancel()
+            snackBarVisible.value = false
+        }
+    }
+
     LaunchedEffect(groupEnterViewModel.sideEffects) {
         groupEnterViewModel.sideEffects.collect { sideEffect ->
             when (sideEffect) {
                 is GroupEnterSideEffect.NavigateUp -> navigateUp()
                 is GroupEnterSideEffect.NavigateToGroup -> navigateToGroup()
+                is GroupEnterSideEffect.ShowErrorDialog -> groupEnterViewModel.showErrorDialog(true)
+                is GroupEnterSideEffect.ShowSnackBar -> onShowFailureSnackBar(
+                    context.getString(sideEffect.message)
+                )
             }
         }
     }
 
+    if (showErrorDialog) {
+        NoostakDialog(
+            dialogType = DialogType.NETWORK_FAILURE,
+            onClick = {
+                groupEnterViewModel.postGroupCode(groupCode)
+            },
+            onDismissRequest = { groupEnterViewModel.showErrorDialog(false) }
+        )
+    }
+
+    when (groupEnterState) {
+        is UiState.Success -> groupEnterViewModel.navigateToGroup()
+        else -> {}
+    }
+
     GroupEnterScreen(
         onBackButtonClick = groupEnterViewModel::navigateUp,
-        onCheckGroupCodeClick = groupEnterViewModel::navigateToGroup
+        onCheckGroupCodeClick = {
+            groupCode = it
+            groupEnterViewModel.postGroupCode(it)
+        },
+        snackBarHostState = snackBarHostState,
+        snackBarVisible = snackBarVisible
     )
 }
 
 @Composable
 fun GroupEnterScreen(
     onBackButtonClick: () -> Unit,
-    onCheckGroupCodeClick: () -> Unit
+    onCheckGroupCodeClick: (String) -> Unit,
+    snackBarHostState: SnackbarHostState,
+    snackBarVisible: MutableState<Boolean>
 ) {
     val focusManager = LocalFocusManager.current
     var groupCode by remember { mutableStateOf("") }
@@ -78,6 +142,26 @@ fun GroupEnterScreen(
                     onBackButtonClick()
                 }
             )
+        },
+        snackbarHost = {
+            AnimatedVisibility(
+                visible = snackBarVisible.value,
+                enter = slideInVertically(initialOffsetY = { it }),
+                exit = slideOutVertically(targetOffsetY = { it })
+            ) {
+                SnackbarHost(
+                    modifier = Modifier.padding(bottom = dimensionResource(id = R.dimen.bottom_padding_snack_bar_non_exist_code)),
+                    hostState = snackBarHostState,
+                    snackbar = { snackBarData ->
+                        NoostakSnackBar(
+                            message = snackBarData.visuals.message,
+                            textStyle = NoostakTheme.typography.c3SemiBold,
+                            textColor = NoostakTheme.colors.red01,
+                            backgroundColor = NoostakTheme.colors.pink
+                        )
+                    }
+                )
+            }
         }
     ) { innerPadding ->
         Column(
@@ -112,7 +196,7 @@ fun GroupEnterScreen(
             NoostakBottomButton(
                 text = stringResource(R.string.btn_group_enter_confirm),
                 isEnabled = groupCode.length == 6,
-                onButtonClick = onCheckGroupCodeClick
+                onButtonClick = { onCheckGroupCodeClick(groupCode) }
             )
         }
     }
@@ -124,7 +208,9 @@ fun GroupEnterPreview() {
     NoostakAndroidTheme {
         GroupEnterScreen(
             onBackButtonClick = {},
-            onCheckGroupCodeClick = {}
+            onCheckGroupCodeClick = {},
+            snackBarHostState = SnackbarHostState(),
+            snackBarVisible = remember { mutableStateOf(true) }
         )
     }
 }
