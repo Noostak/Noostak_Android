f7d8195e78f952b889f6ed7f67d8e756e9f83a34
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 10:12:16 +0900

    #75 [FEAT] ProfileDataSourceImpl 구현

---
+++ data/src/main/java/com/sopt/data/datasourceimpl/ProfileDataSourceImpl.kt
@@ -0,0 +1,14 @@
+package com.sopt.data.datasourceimpl
+
+import com.sopt.data.datasource.ProfileDataSource
+import com.sopt.data.dto.BaseResponse
+import com.sopt.data.dto.response.ResponseGetProfileDto
+import com.sopt.data.service.ProfileApiService
+import javax.inject.Inject
+
+class ProfileDataSourceImpl @Inject constructor(private val profileApiService: ProfileApiService) :
+    ProfileDataSource {
+    override suspend fun getProfile(): BaseResponse<ResponseGetProfileDto> {
+        return profileApiService.getProfile()
+    }
+}
