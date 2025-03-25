3462e1bc449c5c3911f1628434de0e6ec6de11c2
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 12:13:24 +0900

    #75 [MOD] ProfileEntity 데이터 수정

--- domain/src/main/java/com/sopt/domain/entity/ProfileEntity.kt
+++ domain/src/main/java/com/sopt/domain/entity/ProfileEntity.kt
@@ -2,5 +2,7 @@ package com.sopt.domain.entity
 
 data class ProfileEntity(
     val memberName: String = "",
-    val memberProfileImage: String? = null
+    val memberProfileImage: String? = null,
+    val isPermissionGranted: Boolean = false,
+    val isMemberNameCheck: Boolean = false
 )
