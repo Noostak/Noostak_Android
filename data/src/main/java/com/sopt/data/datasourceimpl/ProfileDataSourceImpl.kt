25109d3304c3a5b48d8369d238be826f3497b188
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 12:12:42 +0900

    #75 [FEAT] patchProfile DataSourceImpl 구현

--- data/src/main/java/com/sopt/data/datasourceimpl/ProfileDataSourceImpl.kt
+++ data/src/main/java/com/sopt/data/datasourceimpl/ProfileDataSourceImpl.kt
@@ -4,11 +4,20 @@ import com.sopt.data.datasource.ProfileDataSource
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.response.ResponseGetProfileDto
 import com.sopt.data.service.ProfileApiService
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 import javax.inject.Inject
 
 class ProfileDataSourceImpl @Inject constructor(private val profileApiService: ProfileApiService) :
     ProfileDataSource {
     override suspend fun getProfile(): BaseResponse<ResponseGetProfileDto> {
         return profileApiService.getProfile()
+    }
+
+    override suspend fun patchProfile(
+        memberName: RequestBody,
+        memberProfileImage: MultipartBody.Part?
+    ): BaseResponse<Unit> {
+        return profileApiService.patchProfile(memberName, memberProfileImage)
     }
 }
