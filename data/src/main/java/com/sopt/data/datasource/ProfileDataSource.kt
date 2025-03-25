91a33a0246493d1db89d5455697775e9cbba183c
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 12:12:36 +0900

    #75 [FEAT] patchProfile DataSource 구현

--- data/src/main/java/com/sopt/data/datasource/ProfileDataSource.kt
+++ data/src/main/java/com/sopt/data/datasource/ProfileDataSource.kt
@@ -2,7 +2,13 @@ package com.sopt.data.datasource
 
 import com.sopt.data.dto.BaseResponse
 import com.sopt.data.dto.response.ResponseGetProfileDto
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
 
 interface ProfileDataSource {
     suspend fun getProfile(): BaseResponse<ResponseGetProfileDto>
+    suspend fun patchProfile(
+        memberName: RequestBody,
+        memberProfileImage: MultipartBody.Part?
+    ): BaseResponse<Unit>
 }
