ad3443c69d7d1d5f4395c45cb1b358bdcf512585
Eonji-sw <ejkim0625@gmail.com>
2025-03-25 10:10:51 +0900

    #75 [FEAT] ProfileDataSource Module 추가

--- app/src/main/java/com/sopt/noostak/di/DataSourceModule.kt
+++ app/src/main/java/com/sopt/noostak/di/DataSourceModule.kt
@@ -6,6 +6,7 @@ import com.sopt.data.datasource.AuthDataSource
 import com.sopt.data.datasource.CalendarDataSource
 import com.sopt.data.datasource.ExampleDataSource
 import com.sopt.data.datasource.GroupDataSource
+import com.sopt.data.datasource.ProfileDataSource
 import com.sopt.data.datasource.UserDataSource
 import com.sopt.data.datasourceimpl.AccountDataSourceImpl
 import com.sopt.data.datasourceimpl.AppointmentConfirmDataSourceImpl
@@ -13,6 +14,7 @@ import com.sopt.data.datasourceimpl.AuthDataSourceImpl
 import com.sopt.data.datasourceimpl.CalendarDataSourceImpl
 import com.sopt.data.datasourceimpl.ExampleDataSourceImpl
 import com.sopt.data.datasourceimpl.GroupDataSourceImpl
+import com.sopt.data.datasourceimpl.ProfileDataSourceImpl
 import com.sopt.data.datasourceimpl.UserDataSourceImpl
 import dagger.Binds
 import dagger.Module
@@ -51,4 +53,8 @@ abstract class DataSourceModule {
     @Binds
     @Singleton
     abstract fun bindGroupDataSource(groupDataSourceImpl: GroupDataSourceImpl): GroupDataSource
+
+    @Binds
+    @Singleton
+    abstract fun bindProfileDataSource(profileDataSourceImpl: ProfileDataSourceImpl): ProfileDataSource
 }
