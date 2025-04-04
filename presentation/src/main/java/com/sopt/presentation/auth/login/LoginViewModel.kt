067dbf74dab7d989c053b4ac27263f0b9df0c7e1
youjin09222 <youjin09222@naver.com>
2025-04-05 00:28:57 +0900

    #84 [FEAT] 구글 accessToken 발급하여 서버로 전송

--- presentation/src/main/java/com/sopt/presentation/auth/login/LoginViewModel.kt
+++ presentation/src/main/java/com/sopt/presentation/auth/login/LoginViewModel.kt
@@ -1,6 +1,5 @@
 package com.sopt.presentation.auth.login
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import androidx.annotation.StringRes
@@ -8,7 +7,6 @@ import androidx.lifecycle.viewModelScope
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
@@ -20,16 +18,23 @@ import com.sopt.domain.repository.UserInfoRepository
 import com.sopt.domain.usecase.PostSocialLoginUseCase
 import com.sopt.presentation.R
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import org.json.JSONObject
 import javax.inject.Inject
 import javax.inject.Named
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     @Named("GoogleClientId") private val googleClientId: String,
+    @Named("GoogleClientSecret") private val googleClientSecret: String,
     private val userInfoRepository: UserInfoRepository,
     private val postSocialLoginUseCase: PostSocialLoginUseCase
 ) : BaseViewModel<LoginSideEffect>() {
@@ -37,15 +42,30 @@ class LoginViewModel @Inject constructor(
     private val _showDialog = MutableStateFlow(Pair(DialogType.NETWORK_LOGIN_GOOGLE_FAILURE, false))
     val showDialog: StateFlow<Pair<DialogType, Boolean>> get() = _showDialog
 
-    private lateinit var googleSignInClient: GoogleSignInClient
+    private val _googleSignInIntent = MutableStateFlow<Intent?>(null)
+    val googleSignInIntent: StateFlow<Intent?> get() = _googleSignInIntent
 
     fun showDialog(dialogType: DialogType, isVisible: Boolean) {
         _showDialog.update { it.copy(first = dialogType, second = isVisible) }
     }
 
+    fun prepareGoogleSignInIntent(context: Context) {
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .requestServerAuthCode(googleClientId, true)
+            .build()
+        val client: GoogleSignInClient = GoogleSignIn.getClient(context, gso)
+        _googleSignInIntent.value = client.signInIntent
+    }
+
+    fun clearGoogleSignInIntent() {
+        _googleSignInIntent.value = null
+    }
+
     // Kakao Login
     fun kakaoLogin(context: Context) {
         val loginCallback: (OAuthToken?, Throwable?) -> Unit = this::handleKakaoLoginResult
+
         with(UserApiClient.instance) {
             if (isKakaoTalkLoginAvailable(context)) {
                 loginWithKakaoTalk(context, callback = loginCallback)
@@ -70,34 +90,45 @@ class LoginViewModel @Inject constructor(
         }
     }
 
-    // Google Login
-    fun getGoogleSignInIntent(activity: Activity): Intent {
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .requestServerAuthCode(googleClientId, true)
-            .build()
-        googleSignInClient = GoogleSignIn.getClient(activity, gso)
-        return googleSignInClient.signInIntent
-    }
+    // Get Google AccessToken
+    fun exchangeAuthCodeForAccessToken(authCode: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val requestBody = FormBody.Builder()
+                    .add("grant_type", "authorization_code")
+                    .add("code", authCode)
+                    .add("client_id", googleClientId)
+                    .add("client_secret", googleClientSecret)
+                    .add("redirect_uri", "")
+                    .build()
 
-    fun handleGoogleLoginResult(data: Intent?) {
-        val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-        viewModelScope.launch {
-            runCatching {
-                val account = task.getResult(ApiException::class.java)
-                val authCode = account?.serverAuthCode
+                val request = Request.Builder()
+                    .url("https://oauth2.googleapis.com/token")
+                    .post(requestBody)
+                    .build()
 
-                if (!authCode.isNullOrBlank()) {
-                    postSocialLogin(BEARER + authCode, GOOGLE)
-                    showToast(R.string.toast_google_login_success)
+                val client = OkHttpClient()
+                val response = client.newCall(request).execute()
+                val jsonString = response.body?.string()
+                val json = JSONObject(jsonString ?: "")
+
+                val accessToken = json.optString("access_token")
+
+                if (accessToken.isNotBlank()) {
+                    onGoogleAccessTokenReceived(accessToken)
                 } else {
                     showDialog(DialogType.NETWORK_LOGIN_GOOGLE_FAILURE, true)
                 }
-            }.onFailure {
-                handleError(it, R.string.toast_google_login_failed)
+            } catch (e: Exception) {
                 showDialog(DialogType.NETWORK_LOGIN_GOOGLE_FAILURE, true)
             }
         }
+    }
+
+    // Google Login
+    private fun onGoogleAccessTokenReceived(accessToken: String) {
+        postSocialLogin(BEARER + accessToken, GOOGLE)
+        showToast(R.string.toast_google_login_success)
     }
 
     private fun handleError(error: Throwable?, @StringRes errorMessageResId: Int) {
